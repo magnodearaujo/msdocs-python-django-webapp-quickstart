c3059bbef0c3161676c6435a3a9decf2484382f4
Magno de Araujo <35669207+magnodearaujo@users.noreply.github.com>
2023-10-29 17:49:14 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,13 @@
 provider "azurerm" {}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name="terraform-state"
+    storage_account_name="tfstateaccounttudelft"
+    container_name="tfstatecontainer"
+    key="sandbox/dumbapp.tfstate"
+
+
+
+
+  }
